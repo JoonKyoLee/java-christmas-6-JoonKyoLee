0bf0a70549fe8a5e371e016dec4d0ebce77377f3
JoonKyo <joonkyolee@gmail.com>
2023-11-14 18:18:09 +0900

    refactor, test: null 값과 비어있는 값을 테스트 하는 클래스명 변경 및 정상 입력을 할 경우 예외를 발생하지 않는지 확인하는 테스트 구현

--- src/test/java/christmas/NullAndEmptyValidatorTest.java
+++ src/test/java/christmas/NullAndEmptyValidatorTest.java
@@ -1,6 +1,7 @@
 package christmas;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import christmas.validator.NullAndEmptyValidator;
 import org.junit.jupiter.api.DisplayName;
@@ -8,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class InputViewTest {
+public class NullAndEmptyValidatorTest {
     private static final String NULL_VALUE = null;
     private static final String EMPTY_VALUE = "";
     private static final String SPACE_VALUE = " ";
@@ -16,18 +17,25 @@ public class InputViewTest {
     NullAndEmptyValidator nullAndEmptyValidator = new NullAndEmptyValidator();
 
     @Test
-    @DisplayName("플레이어가 null 값을 입력한 경우 예외를 던진다.")
+    @DisplayName("고객이 null 값을 입력한 경우 예외를 던진다.")
     void nullValueTest() {
         assertThatThrownBy(() -> nullAndEmptyValidator.validateNullAndEmpty(NULL_VALUE))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("플레이어가 비어있는 값을 입력한 경우 예외를 던진다.")
+    @DisplayName("고객이 비어있는 값을 입력한 경우 예외를 던진다.")
     @CsvSource({EMPTY_VALUE, SPACE_VALUE})
     @ParameterizedTest
     void emptyValueTest(String input) {
         assertThatThrownBy(() -> nullAndEmptyValidator.validateNullAndEmpty(input))
                 .isInstanceOf(IllegalArgumentException.class);
 
+    }
+
+    @DisplayName("고객이 null 값 또는 비어있지 않은 값을 입력할 경우 예외가 발생하지 않는다.")
+    @CsvSource({"초코케이크-1", "아이스크림-2", "초코케이크", "3"})
+    @ParameterizedTest
+    void notNullAndNotEmptyValueTest(String input) {
+        assertDoesNotThrow(() -> nullAndEmptyValidator.validateNullAndEmpty(input));
     }
 }
