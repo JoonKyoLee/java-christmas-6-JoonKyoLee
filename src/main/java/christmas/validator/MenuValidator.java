ee49c78455135c2a11bf6cf51686d80e51a6946e
JoonKyo <joonkyolee@gmail.com>
2023-11-14 21:48:31 +0900

    docs, feat(MenuValidator): 음료만 주문한 경우 및 메뉴판에 없는 메뉴를 주문한 경우를 검증하는 메서드 구현

--- src/main/java/christmas/validator/MenuValidator.java
+++ src/main/java/christmas/validator/MenuValidator.java
@@ -1,9 +1,12 @@
 package christmas.validator;
 
+import christmas.domain.menu.Menu;
+import christmas.domain.menu.MenuType;
 import christmas.exception.ErrorMessage;
 import christmas.exception.InputException;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class MenuValidator {
     private static final int MINIMUM_THRESHOLD_PER_MENU = 0;
@@ -15,6 +18,8 @@ public class MenuValidator {
     public void validate(Map<String, Integer> menu) {
         validateEachMenuQuantity(menu);
         validateTotalMenuQuantity(menu);
+        validateMenuNameInMenuList(menu);
+        validateMenuCategories(menu);
     }
 
     private void validateEachMenuQuantity(Map<String, Integer> menu) {
@@ -39,5 +44,34 @@ public class MenuValidator {
         return menu.values()
                 .stream()
                 .reduce(0, Integer::sum) > TOTAL_MENU_THRESHOLD;
+    }
+
+    private void validateMenuNameInMenuList(Map<String, Integer> menu) {
+        if (hasMenuNameOutOfMenuList(menu)) {
+            throw new InputException(ErrorMessage.ORDER_FORMAT_IS_NOT_VALID);
+        }
+    }
+
+    private boolean hasMenuNameOutOfMenuList(Map<String, Integer> menu) {
+        return menu.keySet()
+                .stream()
+                .map(Menu::getMenu)
+                .filter(Objects::nonNull)
+                .count() != menu.size();
+    }
+
+    private void validateMenuCategories(Map<String, Integer> menu) {
+        if (hasOnlyBeverages(menu)) {
+            throw new InputException(ErrorMessage.ORDER_FORMAT_IS_NOT_VALID);
+        }
+    }
+
+    private boolean hasOnlyBeverages(Map<String, Integer> menu) {
+        return menu.keySet()
+                .stream()
+                .allMatch(menuName -> {
+                    Menu menuItem = Menu.getMenu(menuName);
+                    return menuItem.getMenuType().equals(MenuType.BEVERAGE);
+                });
     }
 }
