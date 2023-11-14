a07f5738feaf5b18d63706db33d64e24585adbb1
JoonKyo <joonkyolee@gmail.com>
2023-11-14 19:15:01 +0900

    feat(OutputMessage, OutputView): 고객에게 날짜와 메뉴를 입력받을 문구 출력 메서드 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,2 +1,24 @@
-package christmas.view;public class OutputView {
+package christmas.view;
+
+import static christmas.view.OutputMessage.*;
+
+import christmas.exception.InputException;
+
+public class OutputView {
+
+    public void printGreetingMessage() {
+        System.out.println(GREETING.getMessage());
+    }
+
+    public void askDate() {
+        System.out.println(ASK_DATE.getMessage());
+    }
+
+    public void askMenuAndQuantity() {
+        System.out.println(ASK_MENU_AND_QUANTITY.getMessage());
+    }
+
+    public void printErrorMessage(InputException exception) {
+        System.out.println(exception.getMessage());
+    }
 }
