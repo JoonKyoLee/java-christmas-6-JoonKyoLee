7eaf6cc76c9cad0ef6309026eb55dc29f663d534
JoonKyo <joonkyolee@gmail.com>
2023-11-14 18:29:35 +0900

    feat(ConverInputValidator): 주문 입력 값에 존재하는 콤마(,)와 하이픈(-)을 제거해 리스트로 변환하는 메서드 구현

--- src/main/java/christmas/validator/ConvertInputValidator.java
+++ src/main/java/christmas/validator/ConvertInputValidator.java
@@ -3,7 +3,13 @@ package christmas.validator;
 import christmas.exception.ErrorMessage;
 import christmas.exception.InputException;
 
+import java.util.List;
+
 public class ConvertInputValidator {
+    private static final String SEPARATE_BETWEEN_MENUS_DELIMITER = ",";
+    private static final String SEPARATE_BETWEEN_MENU_AND_PRICE_DELIMITER = "-";
+    private static final String SPACE_DELIMITER = " ";
+
     public ConvertInputValidator() {
     }
 
@@ -17,5 +23,11 @@ public class ConvertInputValidator {
 
     private int parseInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    private List<String> deleteDelimiter(String input) {
+        return List.of(input.replace(SEPARATE_BETWEEN_MENU_AND_PRICE_DELIMITER, SPACE_DELIMITER)
+                .replace(SEPARATE_BETWEEN_MENUS_DELIMITER, SPACE_DELIMITER)
+                .split(SPACE_DELIMITER));
     }
 }
