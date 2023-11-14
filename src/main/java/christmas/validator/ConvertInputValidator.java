ae262f91492f6706531cd0f75a484517355d95b8
JoonKyo <joonkyolee@gmail.com>
2023-11-14 18:50:18 +0900

    feat(ConvertInputValidator): 올바른 형식인지 판단하는 메서드 구현

--- src/main/java/christmas/validator/ConvertInputValidator.java
+++ src/main/java/christmas/validator/ConvertInputValidator.java
@@ -4,12 +4,15 @@ import christmas.exception.ErrorMessage;
 import christmas.exception.InputException;
 
 import java.util.List;
+import java.util.Map;
 
 public class ConvertInputValidator {
     private static final String SEPARATE_BETWEEN_MENUS_DELIMITER = ",";
     private static final String SEPARATE_BETWEEN_MENU_AND_PRICE_DELIMITER = "-";
     private static final String SPACE_DELIMITER = " ";
     private static final String EMPTY_VALUE = "";
+    private static final int DIFFERENCE_BETWEEN_DELIMITERS = 1;
+    private static final int MENU_TO_DELIMITER_RATIO = 2;
 
 
     public ConvertInputValidator() {
@@ -23,8 +26,31 @@ public class ConvertInputValidator {
         }
     }
 
+    public Map<String, Integer> convertOrder(String input) {
+        List<String> deletedDelimiterMenu = deleteDelimiter(input);
+
+        validateMenuInputFormat(deletedDelimiterMenu, input);
+    }
+
+
     private int parseInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    private void validateMenuInputFormat(List<String> deletedDelimiterMenu, String input) {
+        int menuCount = deletedDelimiterMenu.size();
+        int countDelimiterBetweenMenuAndPrice = countDelimiter(input, SEPARATE_BETWEEN_MENU_AND_PRICE_DELIMITER);
+        int countDelimiterBetweenMenus = countDelimiter(input, SEPARATE_BETWEEN_MENUS_DELIMITER);
+
+        if (hasSpaceBetweenInputFormat(input)) {
+            throw new InputException(ErrorMessage.ORDER_FORMAT_IS_NOT_VALID);
+        }
+
+        if (!validateDelimitersAndMenuCount(countDelimiterBetweenMenuAndPrice,
+                countDelimiterBetweenMenus,
+                menuCount)) {
+            throw new InputException(ErrorMessage.ORDER_FORMAT_IS_NOT_VALID);
+        }
     }
 
     private List<String> deleteDelimiter(String input) {
@@ -35,5 +61,29 @@ public class ConvertInputValidator {
 
     private int countDelimiter(String input, String delimiter) {
         return input.length() - input.replace(delimiter, EMPTY_VALUE).length();
+    }
+
+    private boolean hasSpaceBetweenInputFormat(String input) {
+        return input.contains(SPACE_DELIMITER);
+    }
+
+    private boolean validateDelimitersAndMenuCount(
+            int delimiterCountBetweenMenuAndPrice,
+            int delimiterCountBetweenMenus,
+            int menuCount) {
+        return validateDelimiterAndMenuCountRatio(delimiterCountBetweenMenuAndPrice, menuCount)
+                && validateBetweenDelimitersDifference(delimiterCountBetweenMenuAndPrice, delimiterCountBetweenMenus);
+    }
+
+    private boolean validateDelimiterAndMenuCountRatio(
+            int delimiterCountBetweenMenuAndPrice,
+            int menuCount) {
+        return menuCount / delimiterCountBetweenMenuAndPrice == MENU_TO_DELIMITER_RATIO;
+    }
+
+    private boolean validateBetweenDelimitersDifference(
+            int delimiterCountBetweenMenuAndPrice,
+            int delimiterCountBetweenMenus) {
+        return delimiterCountBetweenMenuAndPrice - delimiterCountBetweenMenus == DIFFERENCE_BETWEEN_DELIMITERS;
     }
 }
