febcac28499298be6a9d8cc9ea666dc9a154ec03
JoonKyo <joonkyolee@gmail.com>
2023-11-14 23:40:17 +0900

    docs, feat(Order): 할인 전 총주문 금액을 구하는 메서드와 샴페인을 증정하는지 확인하는 메서드 구현

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,11 +1,33 @@
 package christmas.domain;
 
+import christmas.validator.MenuValidator;
 import java.util.Map;
 
 public class Order {
+    private static final int GRANT_CHAMPAGNE_AMOUNT = 120_000;
+
+    private final MenuValidator menuValidator;
     private final Map<String, Integer> order;
 
-    public Order(Map<String, Integer> order) {
+    public Order(MenuValidator menuValidator, Map<String, Integer> order) {
+        this.menuValidator = menuValidator;
+        validate(order);
+
         this.order = order;
+    }
+
+    private void validate(Map<String, Integer> order) {
+        menuValidator.validate(order);
+    }
+
+    public int calculateTotalAmountBeforeDiscount() {
+        return order.values()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public boolean grantChampagneOnOrder(int totalAmount) {
+        return totalAmount >= GRANT_CHAMPAGNE_AMOUNT;
     }
 }
