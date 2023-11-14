5b4464fedee0be7ab39c4e7a81a7bfd0e7b0df13
JoonKyo <joonkyolee@gmail.com>
2023-11-14 19:27:27 +0900

    feat(Date): 입력 받은 날짜가 범위 안에 존재하는지 판단하는 메서드 구현

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,9 +1,30 @@
 package christmas.domain;
 
+import christmas.exception.ErrorMessage;
+import christmas.exception.InputException;
+
 public class Date {
+    private static final int FIRST_DATE = 1;
+    private static final int LAST_DATE = 31;
+    private static final int CHRISTMAS_DATE = 25;
     private final int date;
 
     public Date(int date) {
+        validate(date);
         this.date = date;
+    }
+
+    private void validate(int date) {
+        validateInputRange(date);
+    }
+
+    private void validateInputRange(int date) {
+        if(isOutOfRange(date)) {
+            throw new InputException(ErrorMessage.DATE_FORMAT_IS_NOT_VALID);
+        }
+    }
+
+    private boolean isOutOfRange(int date) {
+        return date < FIRST_DATE || date > LAST_DATE;
     }
 }
--- src/main/java/christmas/validator/DateValidator.java
+++
@@ -1,9 +0,0 @@
-package christmas.validator;
-
-public class DateValidator {
-    private final int date;
-
-    public DateValidator(int date) {
-        this.date = date;
-    }
-}
