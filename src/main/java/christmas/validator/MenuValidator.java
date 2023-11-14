77b78ed9eb804afd35749de16258e12d235f6761
JoonKyo <joonkyolee@gmail.com>
2023-11-14 21:46:34 +0900

    docs, feat(MenuValidator): 각각 메뉴의 수량과 전체 메뉴의 수량에 대한 유효성을 검증하는 메서드 구현

--- src/main/java/christmas/validator/MenuValidator.java
+++ src/main/java/christmas/validator/MenuValidator.java
@@ -1,11 +1,43 @@
 package christmas.validator;
 
+import christmas.exception.ErrorMessage;
+import christmas.exception.InputException;
+
 import java.util.Map;
 
 public class MenuValidator {
-    private final Map<String, Integer> menu;
+    private static final int MINIMUM_THRESHOLD_PER_MENU = 0;
+    private static final int TOTAL_MENU_THRESHOLD = 20;
 
-    public MenuValidator(Map<String, Integer> menu) {
-        this.menu = menu;
+    public MenuValidator() {
+    }
+
+    public void validate(Map<String, Integer> menu) {
+        validateEachMenuQuantity(menu);
+        validateTotalMenuQuantity(menu);
+    }
+
+    private void validateEachMenuQuantity(Map<String, Integer> menu) {
+        if (!isOverMinimumQuantityPerMenu(menu)) {
+            throw new InputException(ErrorMessage.ORDER_FORMAT_IS_NOT_VALID);
+        }
+    }
+
+    private boolean isOverMinimumQuantityPerMenu(Map<String, Integer> menu) {
+        return menu.values()
+                .stream()
+                .allMatch(quantity -> quantity > MINIMUM_THRESHOLD_PER_MENU);
+    }
+
+    private void validateTotalMenuQuantity(Map<String, Integer> menu) {
+        if (isToTalMenuQuantityOverMaximum(menu)) {
+            throw new InputException(ErrorMessage.ORDER_FORMAT_IS_NOT_VALID);
+        }
+    }
+
+    private boolean isToTalMenuQuantityOverMaximum(Map<String, Integer> menu) {
+        return menu.values()
+                .stream()
+                .reduce(0, Integer::sum) > TOTAL_MENU_THRESHOLD;
     }
 }
