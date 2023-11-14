2532dff8749ec3ba7c77b673f143b578d0bfd1eb
JoonKyo <joonkyolee@gmail.com>
2023-11-15 01:47:54 +0900

    docs, feat(Date): 각각의 이벤트가 적용 가능한지 확인하는 메서드 구현

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -2,11 +2,16 @@ package christmas.domain;
 
 import christmas.exception.ErrorMessage;
 import christmas.exception.InputException;
+import java.time.LocalDate;
 
 public class Date {
+    private static final int YEAR = 2023;
+    private static final int MONTH = 12;
     private static final int FIRST_DATE = 1;
     private static final int LAST_DATE = 31;
     private static final int CHRISTMAS_DATE = 25;
+    private static final int FRIDAY_INDEX = 5;
+    private static final int SUNDAY_INDEX = 7;
     private final int date;
 
     public Date(int date) {
@@ -14,12 +19,33 @@ public class Date {
         this.date = date;
     }
 
+    public boolean canApplyChristmasDDayDiscount() {
+        return date <= CHRISTMAS_DATE;
+    }
+
+    public int calculateDayOfWeek() {
+        LocalDate localDate = LocalDate.of(YEAR, MONTH, date);
+        return localDate.getDayOfWeek().getValue();
+    }
+
+    public boolean isWeekDay(int dayOfWeek) {
+        return dayOfWeek == SUNDAY_INDEX || dayOfWeek < FRIDAY_INDEX;
+    }
+
+    public boolean isWeekend(int dayOfWeek) {
+        return FRIDAY_INDEX <= dayOfWeek && dayOfWeek < SUNDAY_INDEX;
+    }
+
+    public boolean hasStarInCalendar(int dayOfWeek) {
+        return date == CHRISTMAS_DATE || dayOfWeek == SUNDAY_INDEX;
+    }
+
     private void validate(int date) {
         validateInputRange(date);
     }
 
     private void validateInputRange(int date) {
-        if(isOutOfRange(date)) {
+        if (isOutOfRange(date)) {
             throw new InputException(ErrorMessage.DATE_FORMAT_IS_NOT_VALID);
         }
     }
