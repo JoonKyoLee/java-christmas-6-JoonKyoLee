4dda39bff66381b8f37279460c41913baf365930
JoonKyo <joonkyolee@gmail.com>
2023-11-14 18:25:00 +0900

    feat(ChristmasEventManager, ConvertInputValidator): 고객으로부터 날짜를 입력받고 숫자가 아닌 경우에 예외 처리를 하는 메서드 구현

--- src/main/java/christmas/controller/ChristmasEventManager.java
+++ src/main/java/christmas/controller/ChristmasEventManager.java
@@ -1,7 +1,40 @@
 package christmas.controller;
 
+import christmas.domain.Date;
+import christmas.exception.InputException;
+import christmas.validator.ConvertInputValidator;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.function.Supplier;
 
 public class ChristmasEventManager {
-    public ChristmasEventManager() {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final ConvertInputValidator convertInputValidator;
+
+    public ChristmasEventManager(
+            InputView inputView,
+            OutputView outputView,
+            ConvertInputValidator convertInputValidator
+    ) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.convertInputValidator = convertInputValidator;
+    }
+
+    private Date takeDate() {
+        outputView.askDate();
+        String input = inputView.readInput();
+        return new Date(convertInputValidator.convertDate(input));
+    }
+
+    private <T> T repeatUntilReadValidInput(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (InputException exception) {
+                outputView.printErrorMessage(exception);
+            }
+        }
     }
 }
--- src/main/java/christmas/validator/ConvertInputValidator.java
+++ src/main/java/christmas/validator/ConvertInputValidator.java
@@ -1,6 +1,21 @@
 package christmas.validator;
 
+import christmas.exception.ErrorMessage;
+import christmas.exception.InputException;
+
 public class ConvertInputValidator {
     public ConvertInputValidator() {
+    }
+
+    public int convertDate(String input) {
+        try {
+            return parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new InputException(ErrorMessage.DATE_FORMAT_IS_NOT_VALID);
+        }
+    }
+
+    private int parseInt(String input) {
+        return Integer.parseInt(input);
     }
 }
