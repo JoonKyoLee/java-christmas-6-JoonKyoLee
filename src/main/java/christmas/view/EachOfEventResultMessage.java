ecc7660b29077cf4094f10412faa0b98d8c763d2
JoonKyo <joonkyolee@gmail.com>
2023-11-15 21:57:16 +0900

    feat(EachOfEventResultMessage): 각 할인 항목에 맞게 문자열을 만들고 할인 목록이 없다면 없음을 반환하는 메서드 구현

--- src/main/java/christmas/view/EachOfEventResultMessage.java
+++ src/main/java/christmas/view/EachOfEventResultMessage.java
@@ -1,5 +1,8 @@
 package christmas.view;
 
+import java.util.Arrays;
+import java.util.Map;
+
 public enum EachOfEventResultMessage {
     D_DAY_DISCOUNT(1, "크리스마스 디데이 할인:"),
     WEEKDAY_DISCOUNT(2, "평일 할인:"),
@@ -7,7 +10,10 @@ public enum EachOfEventResultMessage {
     SPECIAL_DISCOUNT(4, "특별 할인:"),
     GRANT_EVENT(5, "증정 이벤트:");
 
+    private static final String NOTHING = "없음";
     private static final String SPACE = " ";
+    private static final String PRICE_UNIT = "-%,d원";
+    private static final String NEXT_LINE = "\n";
     private final int messageIndex;
     private final String message;
 
@@ -18,5 +24,28 @@ public enum EachOfEventResultMessage {
 
     public String getMessage() {
         return message + SPACE;
+    }
+
+    public static String makeResultFormOfEachEvent(Map<Integer, Integer> discountPrice) {
+        StringBuilder sb = new StringBuilder();
+
+        discountPrice.forEach((key, value) -> {
+            String message = EachOfEventResultMessage.getEvent(key).getMessage();
+            sb.append(message)
+                    .append(String.format(PRICE_UNIT, value))
+                    .append(NEXT_LINE);
+        });
+
+        if (sb.isEmpty()) {
+            return NOTHING + NEXT_LINE;
+        }
+        return sb.toString();
+    }
+
+    private static EachOfEventResultMessage getEvent(int index) {
+        return Arrays.stream(values())
+                .filter(event -> event.messageIndex == index)
+                .findFirst()
+                .orElse(null);
     }
 }
