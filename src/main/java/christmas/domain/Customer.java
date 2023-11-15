b2f73a80d0e1d2009fd2aec14488e02fbd37c08e
JoonKyo <joonkyolee@gmail.com>
2023-11-15 14:18:45 +0900

    docs, feat(Customer, Order): 주문 금액이 만 원을 넘을 때 각 혜택을 계산하는 메서드 구현

--- src/main/java/christmas/domain/Customer.java
+++ src/main/java/christmas/domain/Customer.java
@@ -1,11 +1,53 @@
 package christmas.domain;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Customer {
+    private static final int MINIMUM_AMOUNT_FOR_EVENT = 10_000;
+
     private final Date date;
     private final Order order;
 
     public Customer(Date date, Order order) {
         this.date = date;
         this.order = order;
+    }
+
+    public Map<Integer, Integer> applyDiscountAmount() {
+        Map<Integer, Integer> appliedDiscount = new LinkedHashMap<>();
+
+        if (order.calculateTotalAmountBeforeDiscount() >= MINIMUM_AMOUNT_FOR_EVENT) {
+            applyDDayDiscount(appliedDiscount);
+            applyWeekdayDiscount(appliedDiscount);
+            applyWeekendDiscount(appliedDiscount);
+            applySpecialDiscount(appliedDiscount);
+        }
+
+        return appliedDiscount;
+    }
+
+    private void applyDDayDiscount(Map<Integer, Integer> appliedDiscount) {
+        if (date.canApplyChristmasDDayDiscount()) {
+            order.calculateChristmasDDayEventDiscount(appliedDiscount, date.countDaysToApplyChristmasDDayDiscount());
+        }
+    }
+
+    private void applyWeekdayDiscount(Map<Integer, Integer> appliedDiscount) {
+        if (date.isWeekday()) {
+            order.weekdayDiscount(appliedDiscount);
+        }
+    }
+
+    private void applyWeekendDiscount(Map<Integer, Integer> appliedDiscount) {
+        if (date.isWeekend()) {
+            order.weekendDiscount(appliedDiscount);
+        }
+    }
+
+    private void applySpecialDiscount(Map<Integer, Integer> appliedDiscount) {
+        if (date.hasStarInCalendar()) {
+            order.specialDiscount(appliedDiscount);
+        }
     }
 }
--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,10 +1,20 @@
 package christmas.domain;
 
+import christmas.domain.menu.Menu;
+import christmas.domain.menu.MenuType;
 import christmas.validator.MenuValidator;
+
 import java.util.Map;
 
 public class Order {
     private static final int GRANT_CHAMPAGNE_AMOUNT = 120_000;
+    private static final int DEFAULT_DISCOUNT_AMOUNT = 1_000;
+    private static final int CHRISTMAS_D_DAY_DISCOUNT_INCREASE_PER_DAY = 100;
+    private static final int CHRISTMAS_D_DAY_EVENT_INDEX = 1;
+    private static final int WEEKDAY_EVENT_INDEX = 2;
+    private static final int WEEKEND_EVENT_INDEX = 3;
+    private static final int SPECIAL_EVENT_INDEX = 4;
+    private static final int GRANT_CHAMPAGNE_INDEX = 5;
 
     private final MenuValidator menuValidator;
     private final Map<String, Integer> order;
@@ -27,7 +37,52 @@ public class Order {
                 .sum();
     }
 
-    public boolean grantChampagneOnOrder(int totalAmount) {
+    public void calculateChristmasDDayEventDiscount(Map<Integer, Integer> appliedDiscount, int elapsedDaysFromDDayEvent) {
+        appliedDiscount.put(
+                CHRISTMAS_D_DAY_EVENT_INDEX
+                , DEFAULT_DISCOUNT_AMOUNT + CHRISTMAS_D_DAY_DISCOUNT_INCREASE_PER_DAY * elapsedDaysFromDDayEvent
+        );
+    }
+
+    public void weekdayDiscount(Map<Integer, Integer> appliedDiscount) {
+        appliedDiscount.put(
+                WEEKDAY_EVENT_INDEX
+                , menuCount(MenuType.DESSERT) * DEFAULT_DISCOUNT_AMOUNT
+        );
+    }
+
+    public void weekendDiscount(Map<Integer, Integer> appliedDiscount) {
+        appliedDiscount.put(
+                WEEKEND_EVENT_INDEX
+                , menuCount(MenuType.MAIN) * DEFAULT_DISCOUNT_AMOUNT
+        );
+    }
+
+    private int menuCount(MenuType menuType) {
+        return order.keySet()
+                .stream()
+                .filter(menuName -> {
+                    Menu menuItem = Menu.getMenu(menuName);
+                    return menuItem.getMenuType().equals(menuType);
+                })
+                .mapToInt(order::get)
+                .sum();
+    }
+
+    public void specialDiscount(Map<Integer, Integer> appliedDiscount) {
+        appliedDiscount.put(
+                SPECIAL_EVENT_INDEX
+                , DEFAULT_DISCOUNT_AMOUNT
+        );
+    }
+
+    public void grantChampagneOnOrder(Map<Integer, Integer> appliedDiscount) {
+        appliedDiscount.put(
+                GRANT_CHAMPAGNE_INDEX
+                , GRANT_CHAMPAGNE_AMOUNT
+        );
+    }
+    public boolean canGrantChampagne(int totalAmount) {
         return totalAmount >= GRANT_CHAMPAGNE_AMOUNT;
     }
 }
